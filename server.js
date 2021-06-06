f8e878ecf84a4c383b8067268c79ebf2667044e6
bertonnipaz <bertonnipaz@gmail.com>
2021-06-05 21:04:43 -0300

    Including the '/' route

--- server.js
+++ server.js
@@ -11,6 +11,14 @@ module.exports = app
 app.use('/menu', menu);
 // app.use('/users', users);
 
+app.get('/', (req, res) => {
+  const body = `
+    <h1>Hello! Welcome to this API</h1>
+    <p>There are just few endpoints available at moment. Here they are: '/menu', '/users', '/user/{name}'. Try it</p>
+    `;
+  res.send(body);
+})
+
 app.get('/users', async (req, res) => {
   const users = await getUsers();
   res.json(users);
